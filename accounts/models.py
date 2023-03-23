cc9ee18e9aa34cbfda60670a5d7dfe280c2787f7
maksymilian03 <mlody2003xd@gmail.com>
2023-03-23 18:53:30 +0100

    added class CustomerUser to acconts/models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class CustomerUser(AbstractUser):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    login = models.CharField(max_length=32)
+    email = models.EmailField(unique=True)
