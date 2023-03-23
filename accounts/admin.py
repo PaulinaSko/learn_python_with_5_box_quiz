b714240ec7f41e8a6ca627400e70d2f4f6cee43a
maksymilian03 <mlody2003xd@gmail.com>
2023-03-23 19:42:46 +0100

    Added class CustomUserAdmin to admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from django.contrib.auth.admin import UserAdmin
+
+from accounts.models import CustomUser
 
 # Register your models here.
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        'username', 'email', 'last_name', 'first_name',
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
