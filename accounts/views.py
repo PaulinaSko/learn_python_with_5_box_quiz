266ac35a583d037d5e2ed7ce11ed95ebaa54cf09
maksymilian03 <mlody2003xd@gmail.com>
2023-03-23 19:44:10 +0100

    Fixed views.py in accounts app

--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,32 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import CustomUser
+from django.contrib.auth import login, get_user_model
+from .forms import CustomUserCreationForm
+from django.contrib.auth.views import LoginView
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views.generic import FormView
+
+User = get_user_model()
 
 
-# Create your views here.
+class CustomLoginView(LoginView):
+    template_name = "accounts/login.html"
+    fields = "__all__"
+    redirect_authenticated_user = True
 
 
-class CustomUserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm):
-        model = CustomUser
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+class RegisterView(FormView):
+    template_name = "accounts/register.html"
+    form_class = CustomUserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy("index")
 
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterView, self).form_valid(form)
 
-class CustomUserChangeForm(UserChangeForm):
-    class Meta:
-        model = CustomUser
-        fields = ('username', 'first_name', 'last_name', 'email')
+    def get(self, *arg, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect("index")
+        return super(RegisterView, self).get(*arg, **kwargs)
