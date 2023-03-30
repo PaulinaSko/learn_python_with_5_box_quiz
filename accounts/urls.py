0b2be9930e69000b9857d7043794b8631b40f964
PaulinaSko <PaulinaSko@users.noreply.github.com>
2023-03-30 18:39:09 +0200

    Deleted app name

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from django.contrib.auth.views import LogoutView
 from .views import CustomLoginView, RegisterView
 
-app_name = 'accounts'
-
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
