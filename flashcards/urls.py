22da70ede4ee7aab5f15036a2143d3f52c980920
PaulinaSko <PaulinaSko@users.noreply.github.com>
2023-03-30 20:52:40 +0200

    Fixed path in urls.py

--- flashcards/urls.py
+++ flashcards/urls.py
@@ -4,6 +4,6 @@ from .views import random_flashcard, FlashcardsView, BlogPageView
 
 urlpatterns = [
     path('', FlashcardsView.as_view(), name='index'),
-    path('flashcards/', BlogPageView.as_view(), name='Leitner_system'),
+    path('Leitner_system', BlogPageView.as_view(), name='Leitner_system'),
     path('flashcards/', random_flashcard, name='flashcard'),
 ]
