bd4261f24a6f7dc39a62a9273e9e7b9487511123
PaulinaSko <PaulinaSko@users.noreply.github.com>
2023-04-07 12:48:01 +0200

    Translated some names into english

--- flashcards/models.py
+++ flashcards/models.py
@@ -11,7 +11,7 @@ class Flashcards(models.Model):
     ID = models.IntegerField(primary_key=True)
     box_ID = models.IntegerField(default=1)
     question = models.CharField(max_length=256)
-    answers = models.CharField(max_length=256, default="Brak odpowiedzi")
+    answers = models.CharField(max_length=256, default="No answer")
 
-    def __str__(self):
+    def __str__(self):  
         return f"Question: {self.question} Answer: {self.answers}"
