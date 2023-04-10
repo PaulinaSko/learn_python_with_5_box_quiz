199c92b3a985a062f31c0cd9d37e2e37d69ee954
PaulinaSko <PaulinaSko@users.noreply.github.com>
2023-04-10 21:47:46 +0200

    Added function to generate pdf

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView
 from .views import CustomLoginView, RegisterView, profile
+from .views import GeneratePdf
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
-    path('profile/', profile, name='profile')
+    path('profile/', profile, name='profile'),
+    path('pdf/', GeneratePdf.as_view(), name='certificate'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -5,8 +5,8 @@ from django.contrib.auth.views import LoginView, FormView
 from django.urls import reverse_lazy
 from .forms import CustomUserCreationForm, LoginForm
 from django.http import HttpResponse
-
-from .models import CustomUser
+from django.views.generic import View
+from .process import html_to_pdf
 
 # Create your views here
 
@@ -49,3 +49,10 @@ class CustomLoginView(LoginView):
 def profile(request):
     user = CustomUserCreationForm(instance=request.user)
     return render(request, 'accounts/profile.html', {"user": user})
+
+
+class GeneratePdf(View):
+    def get(self, request, *args, **kwargs):
+        pdf = html_to_pdf('accounts/result.html')
+
+        return HttpResponse(pdf, content_type='application/pdf')
