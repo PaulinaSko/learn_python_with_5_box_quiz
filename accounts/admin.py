4fbf2ce136f21755e9f2c6eba77436cb30750ff2
maksymilian03 <mlody2003xd@gmail.com>
2023-04-14 22:36:17 +0200

    Registered Profile in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import CustomUser
+from .models import CustomUser, Profile
 # Register your models here.
 
 admin.site.register(CustomUser)
+
+
+@admin.register(Profile)
+class Profile(admin.ModelAdmin):
+    fields = ['user', 'bio']
+    list_display = ['user']
