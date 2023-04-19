02bfaac195ed5d9516952b6a36d9acbfbb97f816
PaulinaSko <PaulinaSko@users.noreply.github.com>
2023-04-19 08:35:34 +0200

    Added new paths for start and finish_page

--- flashcards/urls.py
+++ flashcards/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import random_flashcard, FlashcardsView, BlogPageView, AboutQuiz, Ranking, show_certificate
+from .views import random_flashcard, FlashcardsView, BlogPageView, AboutQuiz, Start, FinishPage, Certificate
 
 urlpatterns = [
     path('', FlashcardsView.as_view(), name='index'),
     path('Leitner_system/', BlogPageView.as_view(), name='Leitner_system'),
     path('about_quiz/', AboutQuiz.as_view(), name='about_quiz'),
-    path('ranking/', Ranking.as_view(), name='ranking'),
     path('flashcards/', random_flashcard, name='flashcard'),
-    path('pdf/', show_certificate, name='certificate'),
+    path('start/', Start.as_view(), name='start'),
+    path('finish_page/', FinishPage.as_view(), name='finish'),
+    path('certificate/', Certificate.as_view(), name='certificate'),
 ]
