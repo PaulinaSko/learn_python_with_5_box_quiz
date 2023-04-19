2aec323b0f62335f27de08876b63a739c5cbbc38
PaulinaSko <PaulinaSko@users.noreply.github.com>
2023-04-19 08:29:22 +0200

    Added re_path to url patterns in learn_python_with_5_box_quiz/urls.py

--- learn_python_with_5_box_quiz/urls.py
+++ learn_python_with_5_box_quiz/urls.py
@@ -13,12 +13,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
+from learn_python_with_5_box_quiz import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include('flashcards.urls')),
-    path("", include('accounts.urls'))
+    path("", include('accounts.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^flashcards/static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
 handler404 = 'flashcards.views.error_404'
